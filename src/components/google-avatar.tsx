f2804378eae6d249a354d61615b3802540ba8fff
PabloRius <pablogrius@gmail.com>
2025-05-27 08:59:51 +0100

    Update header expanded

--- src/components/google-avatar.tsx
+++ src/components/google-avatar.tsx
@@ -23,9 +23,9 @@ export const GoogleAvatar = ({
     return `${parts[0][0]}${parts[parts.length - 1][0]}`.toUpperCase();
   };
   return (
-    <Avatar className="h-8 w-8 border-2 border-white dark:border-gray-800 group-hover:border-pink-200 dark:group-hover:border-pink-900 transition-all">
+    <Avatar className="h-8 w-8 border-2 border-white dark:border-gray-800 group-hover:border-celestial-blue-200 dark:group-hover:border-celestial-blue-900 transition-all">
       <AvatarImage src={src} alt="User" />
-      <AvatarFallback className="bg-gradient-to-br from-pink-400 to-violet-500 text-white">
+      <AvatarFallback className="bg-gradient-to-br from-celestial-blue-400 to-picton-blue-500 text-white">
         {getInitials()}
       </AvatarFallback>
     </Avatar>
