6f7a0cc1d27cb3dcb124004b1cb3128085368ff4
PabloRius <pablogrius@gmail.com>
2025-10-06 11:41:08 +0100

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,59 @@ export const metadata: Metadata = {
   title: "KingSocial",
   description:
     "Connect with the Kingston Univeristy community and level up your social life.",
+  keywords: [
+    "KingSocial",
+    "Kingston",
+    "Social Platform",
+    "Student",
+    "Student Platform",
+    "Kingston University",
+    "Kingston University Student Platform",
+  ],
+  authors: [
+    { name: "Pablo Garcia Rius", url: "https://pablogrius.com" },
+    { name: "KingSocial", url: "https://king-social.vercel.app/" },
+  ],
+  metadataBase: new URL("https://king-social.vercel.app/"),
+  openGraph: {
+    title: "KingSocial - A Kingston University Students' Social Platform",
+    description:
+      "Connect with the Kingston Univeristy community and level up your social life.",
+    url: "https://king-social.vercel.app/",
+    siteName: "KingSocial",
+    images: [
+      {
+        url: "/KingSocialLogo.png",
+        width: 512,
+        height: 512,
+        alt: "KingSocial Logo",
+      },
+    ],
+    locale: "en_GB",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "KingSocial - A Kingston University Students' Social Platform",
+    description:
+      "Connect with the Kingston Univeristy community and level up your social life.",
+    images: ["/KingSocialLogo.png"],
+  },
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/KingSocialLogo.png",
+        href: "/KingSocialLogo.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/KingSocialLogoInverted.png",
+        href: "/KingSocialLogoInverted.png",
+      },
+    ],
+  },
+  robots: { index: true, follow: true },
 };
 
 export default function RootLayout({
